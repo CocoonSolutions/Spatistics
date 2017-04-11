aeb60c76fe484f2a24166cb4bf0dab1fb4d754a8
George Ouzounidis <gouzounidis@tripsta.com>
2017-04-11 19:03:36 +0300

    End of 3rd loesson of shiny tutorial.

--- ui.R
+++ ui.R
@@ -17,12 +17,15 @@ shinyUI(fluidPage(theme = "bootstrap.css",
     ),
 
     # Show a plot of the generated distribution
-    mainPanel(
+    mainPanel(actionButton("action", label = "Action"),
       plotOutput("distPlot")
     )
   ),
   
-  sidebarLayout(
+  # Application title
+  titlePanel("Data from 2017-04-12"),
+  
+  sidebarLayout(position = "right",
     sidebarPanel(
       fileInput('file1', 'Choose CSV File',
                 accept=c('text/csv', 
@@ -43,6 +46,62 @@ shinyUI(fluidPage(theme = "bootstrap.css",
     ),
     mainPanel(
       tableOutput('contents')
+    )
+  ),
+  
+  # Application title
+  titlePanel("Data from 2017-04-13"),
+  sidebarLayout(
+    sidebarPanel(),
+    mainPanel(
+      p("p creates a paragraph of text."),
+      p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+      strong("strong() makes bold text."),
+      em("em() creates italicized (i.e, emphasized) text."),
+      br(),
+      code("code displays your text similar to computer code"),
+      div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+      br(),
+      p("span does the same thing as div, but it works with",
+        span("groups of words", style = "color:blue"),
+        "that appear inside a paragraph.")
+    )
+  ),
+  
+  # Application title
+  titlePanel("Data from 2017-04-14"),
+    sidebarLayout(
+      sidebarPanel(),
+      mainPanel(
+        img(src="bigorb.png", height = 100, width = 100)
+      )
+    ),
+  
+  # Application title
+  titlePanel("Data from 2017-04-15"),
+  sidebarLayout(
+    sidebarPanel(
+      h2("Installation"),
+      p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
+      code('install.packages("shiny")'),
+      br(),
+      br(),
+      br(),
+      br(),
+      img(src = "bigorb.png", height = 72, width = 72),
+      "shiny is a product of ", 
+      span("RStudio", style = "color:blue")
+    ),
+    mainPanel(
+      h1("Introducing Shiny"),
+      p("Shiny is a new package from RStudio that makes it ", 
+        em("incredibly easy"), 
+        " to build interactive web applications with R."),
+      br(),
+      p("For an introduction and live examples, visit the ",
+        a("Shiny homepage.", 
+          href = "http://www.rstudio.com/shiny")),
+      h2("Features")
     )
   )
   
