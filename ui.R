064857ce14f85697a92c3fbf6ba43b755e15e814
George <george@cocoon-solutions.com>
2017-08-18 04:44:09 +0300

    Integrate bootstrap theme "united" and draft navigation bar

--- ui.R
+++ ui.R
@@ -1,108 +1,13 @@
-library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(theme = "bootstrap.css",
-
-  # Application title
-  titlePanel("Data from 2017-04-11"),
-
-  # Sidebar with a slider input for the number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 5,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(actionButton("action", label = "Action"),
-      plotOutput("distPlot")
+                  
+  titlePanel("Story Telling Web App"),
+  
+    navbarPage("My Application",
+      tabPanel("Component 1"),
+      tabPanel("Component 2"),
+      navbarMenu("More",
+        tabPanel("Sub-Component A"),
+        tabPanel("Sub-Component B"))
     )
-  ),
-  
-  # Application title
-  titlePanel("Data from 2017-04-12"),
-  
-  sidebarLayout(position = "right",
-    sidebarPanel(
-      fileInput('file1', 'Choose CSV File',
-                accept=c('text/csv', 
-                         'text/comma-separated-values,text/plain',
-                         '.csv')),
-      tags$hr(),
-      checkboxInput('header', 'Header', TRUE),
-      radioButtons('sep', 'Separator',
-                   c(Comma=',',
-                     Semicolon=';',
-                     Tab='\t'),
-                   ','),
-      radioButtons('quote', 'Quote',
-                   c(None='',
-                     'Double Quote'='"',
-                     'Single Quote'="'"),
-                   '"')
-    ),
-    mainPanel(
-      tableOutput('contents')
-    )
-  ),
-  
-  # Application title
-  titlePanel("Data from 2017-04-13"),
-  sidebarLayout(
-    sidebarPanel(),
-    mainPanel(
-      p("p creates a paragraph of text."),
-      p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
-      strong("strong() makes bold text."),
-      em("em() creates italicized (i.e, emphasized) text."),
-      br(),
-      code("code displays your text similar to computer code"),
-      div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
-      br(),
-      p("span does the same thing as div, but it works with",
-        span("groups of words", style = "color:blue"),
-        "that appear inside a paragraph.")
-    )
-  ),
-  
-  # Application title
-  titlePanel("Data from 2017-04-14"),
-    sidebarLayout(
-      sidebarPanel(),
-      mainPanel(
-        img(src="bigorb.png", height = 100, width = 100)
-      )
-    ),
-  
-  # Application title
-  titlePanel("Data from 2017-04-15"),
-  sidebarLayout(
-    sidebarPanel(
-      h2("Installation"),
-      p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
-      code('install.packages("shiny")'),
-      br(),
-      br(),
-      br(),
-      br(),
-      img(src = "bigorb.png", height = 72, width = 72),
-      "shiny is a product of ", 
-      span("RStudio", style = "color:blue")
-    ),
-    mainPanel(
-      h1("Introducing Shiny"),
-      p("Shiny is a new package from RStudio that makes it ", 
-        em("incredibly easy"), 
-        " to build interactive web applications with R."),
-      br(),
-      p("For an introduction and live examples, visit the ",
-        a("Shiny homepage.", 
-          href = "http://www.rstudio.com/shiny")),
-      h2("Features")
-    )
-  )
-  
 ))
