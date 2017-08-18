dfb7dfd7353a8895dd18981571a1e3d722c4b6a2
George <george@cocoon-solutions.com>
2017-08-18 16:44:12 +0300

    Adding import data module and integrate leflet map tiles

--- server.R
+++ server.R
@@ -1,3 +1,31 @@
-function(input, output, session) {
+library(leaflet)
 
+# allow uploading large files ---
+if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=10000*1024^2)
+
+function(input, output, session) {
+  
+  # import data from csv file ---
+  output$contents <- renderTable({
+    # checking for required file ---
+    req(input$file1)
+    # read uploaded file and save into local var ---
+    data <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    head(data)
+  })
+  
+   output$count <- renderPrint({
+     nrow(data)
+   })
+   
+  # Create and render map ---
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$CartoDB.Positron) %>%
+      setView(lng = 0, lat = 37.45, zoom = 3)
+  })
+  
 }
--- ui.R
+++ ui.R
@@ -1,13 +1,64 @@
+library(leaflet)
 
-shinyUI(fluidPage(theme = "bootstrap.css",
-                  
-  titlePanel("Story Telling Web App"),
-  
-    navbarPage("My Application",
-      tabPanel("Component 1"),
-      tabPanel("Component 2"),
-      navbarMenu("More",
-        tabPanel("Sub-Component A"),
-        tabPanel("Sub-Component B"))
-    )
-))
+# Choices for drop-downs ---
+vars <- c(
+  "Is SuperZIP?" = "superzip",
+  "Centile score" = "centile",
+  "College education" = "college",
+  "Median income" = "income",
+  "Population" = "adultpop"
+)
+
+fluidPage(theme = "bootstrap.css",
+          navbarPage("My Application",
+                     tabPanel("Import Data",
+                              # Sidebar layout with input and output definitions ----
+                              sidebarLayout(
+                                # Sidebar panel for inputs ----
+                                sidebarPanel(
+                                  # Input: Select a file ----
+                                  fileInput("file1", "Choose CSV File:",
+                                            multiple = TRUE,
+                                            accept = c("text/csv",
+                                                       "text/comma-separated-values,text/plain",
+                                                       ".csv")),
+                                  # Input: Checkbox if file has header ----
+                                  checkboxInput("header", "Header", TRUE),
+                                  # Input: Select separator ----
+                                  radioButtons("sep","Choose Separator:",
+                                               choices = c(Comma = ",",
+                                                           Semicolon = ";",
+                                                           Tab = "\t"),
+                                               selected = ","),
+                                  # Input: Select quotes ----
+                                  radioButtons("quote", "Choose Text Quotes:",
+                                               choices = c(None = "",
+                                                           "Double Quote" = '"',
+                                                           "Single Quote" = "'"),
+                                               selected = '"')
+                                ),
+                                # Main panel for displaying outputs ----
+                                mainPanel(
+                                  # Output: Data file ----
+                                  tableOutput("contents")
+                                )
+                              )
+                     ),
+                     tabPanel("Visualize",
+                              div(class="outer",
+                                  
+                                  tags$head(
+                                    # Include our custom CSS
+                                    includeCSS("styles.css"),
+                                    includeScript("gomap.js")
+                                  ),
+                                  
+                                  leafletOutput("map", width="100%", height="100%")
+                              )
+                     ),
+                     tabPanel("Component 2"),
+                     navbarMenu("More",
+                                tabPanel("Sub-Component A"),
+                                tabPanel("Sub-Component B"))
+          )
+)
