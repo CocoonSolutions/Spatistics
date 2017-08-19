7c35448db74e3d67cb61bb2ea9d3e2a08b858f65
George <george@cocoon-solutions.com>
2017-08-19 23:08:23 +0300

    Create single page html in shiny app.

--- server.R
+++ server.R
@@ -5,22 +5,6 @@ if(Sys.getenv('SHINY_PORT') == "") options(shiny.maxRequestSize=10000*1024^2)
 
 function(input, output, session) {
   
-  # import data from csv file ---
-  output$contents <- renderTable({
-    # checking for required file ---
-    req(input$file1)
-    # read uploaded file and save into local var ---
-    data <- read.csv(input$file1$datapath,
-                   header = input$header,
-                   sep = input$sep,
-                   quote = input$quote)
-    head(data)
-  })
-  
-   output$count <- renderPrint({
-     nrow(data)
-   })
-   
   # Create and render map ---
   output$map <- renderLeaflet({
     leaflet() %>%
--- ui.R
+++ ui.R
@@ -1,64 +1,114 @@
 library(leaflet)
 
-# Choices for drop-downs ---
-vars <- c(
-  "Is SuperZIP?" = "superzip",
-  "Centile score" = "centile",
-  "College education" = "college",
-  "Median income" = "income",
-  "Population" = "adultpop"
-)
-
 fluidPage(theme = "bootstrap.css",
-          navbarPage("My Application",
-                     tabPanel("Import Data",
-                              # Sidebar layout with input and output definitions ----
-                              sidebarLayout(
-                                # Sidebar panel for inputs ----
-                                sidebarPanel(
-                                  # Input: Select a file ----
-                                  fileInput("file1", "Choose CSV File:",
-                                            multiple = TRUE,
-                                            accept = c("text/csv",
-                                                       "text/comma-separated-values,text/plain",
-                                                       ".csv")),
-                                  # Input: Checkbox if file has header ----
-                                  checkboxInput("header", "Header", TRUE),
-                                  # Input: Select separator ----
-                                  radioButtons("sep","Choose Separator:",
-                                               choices = c(Comma = ",",
-                                                           Semicolon = ";",
-                                                           Tab = "\t"),
-                                               selected = ","),
-                                  # Input: Select quotes ----
-                                  radioButtons("quote", "Choose Text Quotes:",
-                                               choices = c(None = "",
-                                                           "Double Quote" = '"',
-                                                           "Single Quote" = "'"),
-                                               selected = '"')
-                                ),
-                                # Main panel for displaying outputs ----
-                                mainPanel(
-                                  # Output: Data file ----
-                                  tableOutput("contents")
-                                )
-                              )
+          
+          # Include our custom CSS  and js ---
+          tags$head(
+            #includeCSS("styles.css"),
+            includeScript("gomap.js")
+          ),
+          
+          # 1 ---
+          fluidRow(
+            column(width = 12,
+                   img(src="olives.jpg", height = "100%", width = "100%")
+                   
+            )
+          ),
+          
+          # 2 ---
+          fluidRow(
+            column(width = 1
+            ),
+            
+            # 2.1 ---
+            column(width = 10,
+                   h2("This is the main tiltle of the story."),
+                   h4("This is a small paragraph with questions regarding the story. This is a small paragraph with questions regarding the story. This is a small paragraph with questions regarding the story. This is a small paragraph with questions regarding the story. This is a small paragraph with questions regarding the story. This is a small paragraph with questions regarding the story."),
+                   
+                   # 2.1.1 ---
+                   # ----------------------------------------
+                   # Point 1 --------------------------------
+                   # ----------------------------------------
+                   fluidRow(
+                     column(width = 12,
+                            tags$h1("_____________", style = "color:red; text-align:center; font-size: 20px;"),
+                            h2(span(". ", style = "background-color:red; color:red; font-size: 120%;"),
+                               span("1 ", style = "background-color:red; font-size: 120%;"),
+                               span(".", style = "background-color:red; color:red; font-size: 120%;"),
+                               span("-", style = "color:white; font-size: 120%;"),"This is the main tiltle of the paragraph.")
+                     )
+                   ),
+                   fluidRow(
+                     column(width = 4,
+                            h4("2These changes have increased wealth inequality significantly. In 1963, families near the top had six times the wealth (or, $6 for every $1) of families in the middle. By 2013, they had 12 times the wealth of families in the middle."),
+                            h4(tags$ul(
+                              tags$li("First list item First list item First list item First list item First list item"), 
+                              br(),
+                              tags$li("Second list item First list item First list item First list item First list item First list item First list item First list item First list item First list item First list item"), 
+                              br(),
+                              tags$li("Third list item")
+                            ))
                      ),
-                     tabPanel("Visualize",
-                              div(class="outer",
-                                  
-                                  tags$head(
-                                    # Include our custom CSS
-                                    includeCSS("styles.css"),
-                                    includeScript("gomap.js")
-                                  ),
-                                  
-                                  leafletOutput("map", width="100%", height="100%")
-                              )
+                     column(width = 8,
+                            h4("3These changes have increased wealth inequality significantly. In 1963, families near the top had six times the wealth (or, $6 for every $1) of families in the middle. By 2013, they had 12 times the wealth of families in the middle."),
+                            leafletOutput("map","100%", "400px")
+                     )
+                   ),
+                   # ----------------------------------------
+                   # Point 2 --------------------------------
+                   # ----------------------------------------
+                   fluidRow(
+                     column(width = 12,
+                            tags$h1("_____________", style = "color:red; text-align:center; font-size: 20px;"),
+                            h2(span(". ", style = "background-color:red; color:red; font-size: 120%;"),
+                               span("2 ", style = "background-color:red; font-size: 120%;"),
+                               span(".", style = "background-color:red; color:red; font-size: 120%;"),
+                               span("-", style = "color:white; font-size: 120%;"),"This is the main tiltle of the paragraph.")
+                     )
+                   ),
+                   fluidRow(
+                     column(width = 4,
+                            h4("2These changes have increased wealth inequality significantly. In 1963, families near the top had six times the wealth (or, $6 for every $1) of families in the middle. By 2013, they had 12 times the wealth of families in the middle.")
                      ),
-                     tabPanel("Component 2"),
-                     navbarMenu("More",
-                                tabPanel("Sub-Component A"),
-                                tabPanel("Sub-Component B"))
+                     column(width = 8,
+                            h4("3These changes have increased wealth inequality significantly. In 1963, families near the top had six times the wealth (or, $6 for every $1) of families in the middle. By 2013, they had 12 times the wealth of families in the middle."),
+                            tags$code("Tidy data sets are all the same. Each messy data set is messy in its own way.", cite = "Hadley Wickham")
+                     )
+                   ),
+                   # ----------------------------------------
+                   # Conclusion -----------------------------
+                   # ---------------------------------------- 
+                   fluidRow(
+                     column(width = 12,
+                            tags$h1("_____________", style = "color:red; text-align:center; font-size: 20px;"),
+                            h2(span(".. ", style = "background-color:red; color:red; font-size: 120%;"),
+                               span("...", style = "background-color:red; color:red; font-size: 120%;"),
+                               span("-", style = "color:white; font-size: 120%;"),"This is the conclusion."),
+                            p("p creates a paragraph of text."),
+                            p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph."),
+                            strong("strong() makes bold text."),
+                            em("em() creates italicized (i.e, emphasized) text."),
+                            br(),
+                            code("code displays your text similar to computer code"),
+                            div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+                            br(),
+                            p("span does the same thing as div, but it works with",
+                              span("groups of words", style = "color:blue"),
+                              "that appear inside a paragraph.")
+                     )
+                   )
+            ),
+            column(width = 1
+            )
+          ),
+          # 3 ---
+          fluidRow(
+            column(width = 12,
+                   img(src="olives.jpg", height = "100%", width = "100%")
+            )
           )
 )
+
+
+
